944cb9ee27bc210c990374dbec35df4acbc39e32
Steffany Sympson <ssy.marques@gmail.com>
2021-09-23 19:50:54 -0300

    exercícios JS

---
+++ Exercícios de JS/Resolução Comentanda/1 Console.js
@@ -0,0 +1,3 @@
+// Frase na tela - Implemente um programa que escreve na tela uma frase
+
+console.log("Olá Mundo! O primeiro programa a gente nunca esquece!")
---
+++ Exercícios de JS/Resolução Comentanda/2 Etiqueta.js
@@ -0,0 +1,2 @@
+// Etiqueta - Elabore um programa que escreve seu nome completo na primeira linha, seu endereço na segunda e o CEP e telefone na terceira.
+
